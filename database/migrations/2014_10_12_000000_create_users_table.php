aafcdcaffadf81deb30684f0ecf049aadf521e30
ozdgnertugrul@gmail.com <ozdgnertugrul@gmail.com>
2020-08-31 18:22:01 +0300

    User veritabanı hazırlanması

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,8 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->tinyInteger('active')->default(0);
+            $table->dateTime('last_login')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
